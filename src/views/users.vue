9e170257a5b9f7d6286de437eaff86299e172672
naxo25 <y.labra.25@gmail.com>
2021-08-09 20:13:40 -0400

    display users from firebase && click display user chat

---
+++ src/views/users.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="q-pa-md">
+    <q-list v-for="user, key in store.state.users" :key="key" flat>
+      <q-item @click='loadChat(key)' class="q-mb-sm" clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar>
+            <img :src="user.img || 'quasar-logo.svg'">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+          <q-item-label caption lines="1">{{ user.email }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <!-- <q-icon name="chat_bubble" color="grey" /> -->
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default {
+  setup () {
+    const store= inject('store')
+    const router = useRouter()
+
+    const loadChat = (otherKey) => {
+      router.push('/Chat/' + otherKey)
+    }
+
+    return {
+      store,
+      loadChat
+    }
+  }
+}
+</script>
